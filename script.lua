ab9188a341f04332dd994bf4242704ba097525ce
Regall-art <satrioalfriza@gmail.com>
2026-02-15 17:04:49 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,7 +5,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Driving Empire - Working",
+   Name = "Nyemek Hub",
    LoadingTitle = "Driving Empire Auto Farm",
    LoadingSubtitle = "Loading...",
    ConfigurationSaving = {Enabled = false},
@@ -200,3 +200,4 @@ Rayfield:Notify({
 })
 
 print("Driving Empire script loaded!")
+
